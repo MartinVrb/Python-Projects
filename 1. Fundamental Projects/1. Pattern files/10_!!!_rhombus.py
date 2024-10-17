09a75fe21da8f1cff3c2890ca270d42367991c37
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-10-17 23:21:59 +0300

    Add files via upload

---
+++ 1. Fundamental Projects/1. Pattern files/10_!!!_rhombus.py
@@ -0,0 +1,13 @@
+number = int(input())
+for i in range(number):
+    print(" " * (number - i), end="")
+
+    for j in range(i + 1):
+        print("* ", end="")
+    print()
+
+for i in range(number, -1, -1):
+    print(" " * (number - i), end="")
+    for j in range(i + 1):
+        print("* ", end="")
+    print()
